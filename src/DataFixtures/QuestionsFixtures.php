208662148000023f2faa972c507f322e12b1043d
jeremyHzn <heizmann.jeremy@gmail.com>
2023-05-26 10:19:49 +0200

    copy merge wtf

--- src/DataFixtures/QuestionsFixtures.php
+++ src/DataFixtures/QuestionsFixtures.php
@@ -4,16 +4,25 @@ namespace App\DataFixtures;
 
 use App\Entity\Questions;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
 /**
  * Class QuestionsFixtures
  */
-class QuestionsFixtures extends Fixture
+final class QuestionsFixtures extends Fixture implements DependentFixtureInterface
 {
-    public const COUNT_OF_QUESTIONS = 3;
+    public const COUNT_OF_QUESTIONS = 4;
 
     private ObjectManager $manager;
+
+    public function getDependencies(): \Traversable|array
+    {
+        return [
+            TypesFixtures::class,
+            ValuesFixtures::class,
+        ];
+    }
 
     /**
      * @param ObjectManager $manager
@@ -43,38 +52,94 @@ class QuestionsFixtures extends Fixture
      * persist the child question
      * flush the manager
      */
-    public function loadQuestion(Questions $type, Questions $value)
+    public function loadQuestions(): void
     {
-        $parent = $this->createQuestion('SAV', null, $manager);
+        $parent = null;
 
-        $this->createQuestion('La personne que vous avez eu au téléphone était-elle audible ?', $parent, $manager);
-        $this->createQuestion('La personne que vous avez eu au téléphone était-elle aimable ?', $parent, $manager);
-        $this->createQuestion('La personne que vous avez eu au téléphone a-t-elle réussi à résoudre votre problème ?', $parent, $manager);
+        $subjects = $this->dataProvider();
+
+        foreach ($subjects as $key => $subject) {
+            $typeReferenceKey = TypesFixtures::TYPE_REFERENCE_PREFIX . $key;
+
+            $valueReferenceKey = ValuesFixtures::VALUE_REFERENCE_PREFIX . $key;
+
+            if ($key === 0) {
+                $parent = $this->createQuestion(
+                    subject: $subject,
+                    typeReferenceKey: $typeReferenceKey,
+                    valueReferenceKey: $valueReferenceKey
+                );
+
+                continue;
+            }
+
+            $this->createQuestion(
+                subject: $subject,
+                parent: $parent,
+                typeReferenceKey: $typeReferenceKey,
+                valueReferenceKey: $valueReferenceKey
+            );
+        }
     }
 
-    /**
-     * @param string $subject
-     * @param Questions|null $parent
-     * @return Questions
-     * function createQuestion is private because it is only used in this class
-     * create a new question with the value of the dataProvider
-     * set the subject of the question
-     * set the parent of the question
-     * persist the question
-     * return the question
-     */
-    public function createQuestion(string $subject, ?Questions $parent): Questions
+    public function createQuestion(
+        string     $subject,
+        ?Questions $parent = null,
+        ?string    $typeReferenceKey = null,
+        ?string    $valueReferenceKey = null
+    ): Questions
     {
-        $type = $this->getReference(TypesFixtures::TYPE_REFERENCE_PREFIX);
-        $value = $this->getReference(ValuesFixtures::VALUE_REFERENCE_PREFIX);
+        [
+            'type' => $type,
+            'value' => $value,
+        ] = $this->getTypesAndValuesInstancesOrThrowException(
+            $typeReferenceKey,
+            $valueReferenceKey
+        );
 
-        $question = new Questions($value, $type);
+        $question = new Questions($type, $value);
 
-        $question->setSubject($subject);
-        $question->setParent($parent);
-        $this->manager->persist($question);
+        $question
+            ->setCategory('YOU HAVE TO CHECK THIS BRO')
+            ->setSubject($subject)
+            ->setParent($parent);
 
+        $this
+            ->manager
+            ->persist($question);
         return $question;
     }
 
+    private function dataProvider(): \Traversable|array
+    {
+        return [
+            'SAV',
+            'La personne que vous avez eu au téléphone était-elle audible ?',
+            'La personne que vous avez eu au téléphone était-elle aimable ?',
+            'La personne que vous avez eu au téléphone a-t-elle réussi à résoudre votre problème ?',
+        ];
+    }
+
+    private function getTypesAndValuesInstancesOrThrowException(
+        string $typeReferenceKey,
+        string $valueReferenceKey
+    ): \Traversable|array
+    {
+        $type = $this->getReference($typeReferenceKey);
+
+        $value = $this->getReference($valueReferenceKey);
+
+        if (
+            $type instanceof Types === false
+            ||
+            $value instanceof Values === false
+        ) {
+            throw new LogicException('You have a problem on references.');
+        }
+
+        return [
+            'type' => $type,
+            'value' => $value,
+        ];
+    }
 }
